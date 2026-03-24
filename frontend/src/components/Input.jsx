231ef9a1041f718d79fad48f693f3c05c46729ae
Bhavesh Kumar <bhaveshaadra26@gmail.com>
2026-03-24 23:32:32 +0530

    feat: add FloatingShape and Input components
    - Introduced FloatingShape component for animated background shapes using framer-motion.
    - Created Input component with customizable icon and label, including password validation message.

---
+++ frontend/src/components/Input.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Input = ({ icon: Icon, labelName, ...props }) => {
+  return (
+    <div className="form-control w-full">
+      <label className="label">
+        <span className="label-text">{labelName}</span>
+      </label>
+      <div className="relative">
+        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+          <Icon className="size-5 text-blue-500" />
+        </div>
+        <input
+          {...props}
+          className="input input-bordered w-full pl-10 pr-3 py-2 bg-gray-800 bg-opacity-50 rounded-lg border border-gray-700 focus:border-primary focus:ring-2 focus:primary text-white placeholder-gray-400 transition duration-200"
+          required
+        />
+      </div>
+      {props.type === "password" && (
+        <p className="text-xs opacity-70 mt-1">
+          Password must be at least 6 characters long
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default Input;
