4abc597c7fc37dd77d0cae3b73b60def405c9584
Bhavesh Kumar <bhaveshaadra26@gmail.com>
2026-04-02 16:19:03 +0530

    fix: update OnboardingPage background color and adjust height for improved layout

--- frontend/src/pages/OnboardingPage.jsx
+++ frontend/src/pages/OnboardingPage.jsx
@@ -38,11 +38,11 @@ const OnboardingPage = () => {
 
   return (
     <div
-      className="h-[100dvh] overflow-y-auto bg-red-400 bg-base-100 flex lg:items-center justify-center p-4 "
+      className="h-[100dvh] overflow-y-auto bg-gray-700 bg-base-100 flex lg:items-center justify-center p-4 "
       data-theme="dark"
     >
-      <div className="bg-base-200 rounded-2xl h-[102dvh] md:rounded-3xl w-full mb-6 max-w-3xl shadow-xl">
-        <div className="p-6 sm:p-8">
+      <div className="bg-base-200 rounded-2xl h-[103dvh] md:rounded-3xl w-full mb-6 max-w-3xl shadow-xl">
+        <div className="p-6 py-8  sm:p-8">
           <h1 className="Capitalize text-2xl sm:text-3xl font-bold text-center mb-6">
             Complete Your Profile
           </h1>
