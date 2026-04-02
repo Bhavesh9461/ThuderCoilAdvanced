6a6559004a2fcd209abbea7e99452defba3c6925
Bhavesh Kumar <bhaveshaadra26@gmail.com>
2026-04-02 08:59:10 +0530

    feat: add package.json and .gitignore files, update app.js for production static serving

--- backend/src/app.js
+++ backend/src/app.js
@@ -32,8 +32,12 @@ app.use("/api/users", userRoutes)
 /**
  * @TODO
  */
-// if(process.env.NODE_ENV === "production"){
-//     app.use()
-// }
+if(process.env.NODE_ENV === "production"){
+      app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+      app.get("*", (req,res)=>{
+          res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+      })
+  }
 
 export default app
