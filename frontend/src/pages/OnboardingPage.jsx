93d97dce22f1255406c7d04af2fa7d044e345169
Bhavesh Kumar <bhaveshaadra26@gmail.com>
2026-04-02 17:49:19 +0530

    fix: update OnboardingPage layout for improved alignment and visual consistency

--- frontend/src/pages/OnboardingPage.jsx
+++ frontend/src/pages/OnboardingPage.jsx
@@ -38,10 +38,10 @@ const OnboardingPage = () => {
 
   return (
     <div
-      className="h-[100dvh] overflow-y-auto bg-gray-700 bg-base-100 flex lg:items-center justify-center p-4 "
+      className="h-[100dvh] overflow-y-auto bg-gray-700 bg-base-100 flex items-start justify-center p-4 "
       data-theme="dark"
     >
-      <div className="bg-base-200 rounded-2xl h-[103dvh] md:rounded-3xl w-full max-w-3xl shadow-xl">
+      <div className="bg-base-200 rounded-2xl md:rounded-3xl w-full max-w-3xl shadow-xl">
         <div className="p-6 sm:p-8">
           <h1 className="Capitalize text-2xl sm:text-3xl font-bold text-center mb-6">
             Complete Your Profile
